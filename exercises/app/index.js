167606dbdb6b547a18ab427a3501a3cec5dfcce2
KryptKode <paulakhamiogu@gmail.com>
2020-08-25 20:33:13 +0100

    set up app server

--- exercises/app/index.js
+++ exercises/app/index.js
@@ -1,27 +1,32 @@
 const express = require('express')
 const morgan = require('morgan')
 const connect = require('../connect')
-const {json, urlencoded} = require('body-parser')
+const { json, urlencoded } = require('body-parser')
 const app = express()
 const Todo = require('./todo')
 
 app.use(morgan('dev'))
-app.use(urlencoded({extended: true}))
+app.use(urlencoded({ extended: true }))
 app.use(json())
 
 app.get('/todo/:id', async (req, res) => {
   const todoId = req.params.id
+  const todo = await Todo.findById(todoId).lean().exec()
+  res.status(200).json(todo)
 })
 
 app.get('/todos', async (req, res) => {
-
+  const todos = await Todo.find({}).lean().exec()
+  res.status(200).json(todos)
 })
 
-app.post('/todo', async () => {
+app.post('/todo', async (req, res) => {
   const todoToCreate = req.body.todo
+  const todo = await Todo.create(todoToCreate)
+  res.status(200).json(todo.toJSON())
 })
 
-connect(/**add mongo url here */)
+connect('mongodb://localhost:27017/expresss')
   .then(() => app.listen(4000, () => {
     console.log('server on http://localhost:4000')
   }))
